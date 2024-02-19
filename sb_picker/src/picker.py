13a17fe0544fb58269bdc75030488982ca047e43
Jannes Münchmeyer <munchmej@univ-grenoble-alpes.fr>
2024-02-19 20:13:03 +0100

    Refactored S3MongoSBPicker

--- sb_picker/src/picker.py
+++ sb_picker/src/picker.py
@@ -2,7 +2,8 @@ import argparse
 import asyncio
 import datetime
 import logging
-from typing import Iterable, Optional
+import time
+from typing import Any, Iterable, Mapping, Optional
 
 import numpy as np
 import obspy
@@ -10,7 +11,10 @@ import pymongo
 import seisbench
 import seisbench.models as sbm
 import seisbench.util as sbu
-from pymongo.errors import DuplicateKeyError
+from bson import ObjectId
+from pymongo.collection import Collection
+from pymongo.errors import BulkWriteError, DuplicateKeyError
+from pymongo.results import InsertOneResult
 from s3fs import S3FileSystem
 
 logger = logging.getLogger("sb_picker")
@@ -26,7 +30,7 @@ def main():
     parser.add_argument("--s3_format", type=str, default="ncedc")
     parser.add_argument("--db_uri", type=str, required=True)
     parser.add_argument(
-        "--db_container", type=str, required=True, help="db.container for MongoDB"
+        "--collection", type=str, required=True, help="The collection for MongoDB"
     )
     parser.add_argument(
         "--stations",
@@ -56,135 +60,89 @@ def main():
     parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
 
-    picker = S3MongoSBPicker(**vars(args))
+    if args.debug:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.DEBUG)
+
+    db = SeisBenchCollection(args.db_uri, args.collection)
+    s3 = S3DataSource(
+        args.s3, args.s3_format, args.stations, args.start, args.end, args.components
+    )
+    picker = S3MongoSBPicker(
+        s3=s3,
+        db=db,
+        model=args.model,
+        weight=args.weight,
+        p_threshold=args.p_threshold,
+        s_threshold=args.s_threshold,
+        data_queue_size=args.data_queue_size,
+        pick_queue_size=args.pick_queue_size,
+    )
 
     picker.run()
 
 
-class S3MongoSBPicker:
+class SeisBenchCollection(pymongo.MongoClient):
+    def __init__(self, db_uri: str, collection: str, **kwargs: Any) -> None:
+        super().__init__(db_uri, **kwargs)
+
+        self.db_uri = db_uri
+        self._collection = super().__getitem__(collection)
+
+        self.dbs = {"picks", "stations", "sb_runs"}
+        self._setup()
+
+    def _setup(self):
+        pick_db = self["picks"]
+        if "unique_index" not in pick_db.index_information():
+            pick_db.create_index(
+                ["trace_id", "phase", "time"], unique=True, name="unique_index"
+            )
+
+    def __getitem__(self, item) -> Collection[Mapping[str, Any] | Any]:
+        if item not in self.dbs:
+            raise KeyError(
+                f"Database '{item}' not available. Possible options are: "
+                + ", ".join(self.dbs)
+            )
+        return self._collection[item]
+
+
+class S3DataSource:
     def __init__(
         self,
         s3: str,
         s3_format: str,
-        db_uri: str,
-        db_container: str,
         stations: str,
         start: datetime.date,
         end: datetime.date,
         components: str,
-        model: str,
-        weight: str,
-        p_threshold: float,
-        s_threshold: float,
-        data_queue_size: Optional[int],
-        pick_queue_size: Optional[int],
-        debug: bool = False,
     ):
-        if debug:
-            handler = logging.StreamHandler()
-            formatter = logging.Formatter(
-                "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-            )
-            handler.setFormatter(formatter)
-            logger.addHandler(handler)
-            logger.setLevel(logging.DEBUG)
-
-        self.model = self.create_model(model, weight, p_threshold, s_threshold)
-        self.stations = stations.split(",")
-
         self.s3 = s3
         self.s3_format = s3_format
-        self.db_uri = db_uri
-        self.container_db = db_container
         self.start = start
         self.end = end
-        self.data_queue_size = data_queue_size
-        self.pick_queue_size = pick_queue_size
         self.components = components
+        self.stations = stations.split(",")
 
-        self.metadata = {
-            "model": model,
-            "weight": weight,
-            "p_threshold": p_threshold,
-            "s_threshold": s_threshold,
-            "components_loaded": components,
-            "seisbench_version": seisbench.__version__,
-            "weight_version": self.model.weights_version,
-        }
-
-        self._db = None
-
-    @property
-    def db(self) -> pymongo.MongoClient:
-        if self._db is None:
-            self._db = pymongo.MongoClient(self.db_uri)
-            db, container = self.container_db.split(".")
-            if "unique_index" not in self._db[db][container].index_information():
-                self._db[db][container].create_index(
-                    ["trace_id", "phase", "time"], unique=True, name="unique_index"
-                )
-
-        return self._db
-
-    @property
-    def collection(self) -> pymongo.collection.Collection:
-        db, container = self.container_db.split(".")
-        return self.db[db][container]
-
-    @staticmethod
-    def create_model(
-        model: str, weight: str, p_threshold: float, s_threshold: float
-    ) -> sbm.WaveformModel:
-        model = sbm.__getattribute__(model).from_pretrained(weight)
-        model.default_args["P_threshold"] = p_threshold
-        model.default_args["S_threshold"] = s_threshold
-        return model
-
-    @staticmethod
-    def parse_stations(stations: str) -> Iterable:
-        pass
-
-    def run(self):
+    def load_data(self) -> obspy.Stream:
         days = np.arange(self.start, self.end, datetime.timedelta(days=1))
-        tasks = asyncio.Queue()
+        fs = S3FileSystem(anon=True)
+
         for station in self.stations:
             for day in days:
-                tasks.put_nowait((station, day))
-        tasks.put_nowait(None)
-
-        asyncio.run(self._run_async(tasks))
-
-    async def _run_async(self, tasks: asyncio.Queue):
-        data = asyncio.Queue(self.data_queue_size)
-        picks = asyncio.Queue(self.pick_queue_size)
-
-        task_load = self._load_data(tasks, data)
-        task_pick = self._pick_data(data, picks)
-        task_db = self._write_pick_to_db(picks)
-
-        await asyncio.gather(task_load, task_pick, task_db)
-
-    async def _load_data(
-        self,
-        tasks: asyncio.Queue[tuple[str, datetime.date] | None],
-        data: asyncio.Queue[obspy.Stream | None],
-    ):
-        fs = S3FileSystem(anon=True)
-        while True:
-            task = await tasks.get()
-            if task is None:
-                await data.put(None)
-                break
-
-            station, date = task
-            logger.debug(f"Loading {station} - {date}")
-
-            stream = obspy.Stream()
-            for uri in self._generate_waveform_uris(
-                station, date.astype(datetime.datetime)
-            ):
-                stream += await asyncio.to_thread(self._read_from_s3, fs, uri)
-            await data.put(stream)
+                logger.debug(f"Loading {station} - {day}")
+                stream = obspy.Stream()
+                for uri in self._generate_waveform_uris(
+                    station, day.astype(datetime.datetime)
+                ):
+                    stream += self._read_from_s3(fs, uri)
+                yield stream
 
     @staticmethod
     def _read_from_s3(fs, uri) -> obspy.Stream:
@@ -209,6 +167,72 @@ class S3MongoSBPicker:
 
         return uris
 
+
+class S3MongoSBPicker:
+    def __init__(
+        self,
+        s3: S3DataSource,
+        db: SeisBenchCollection,
+        model: str,
+        weight: str,
+        p_threshold: float,
+        s_threshold: float,
+        data_queue_size: Optional[int],
+        pick_queue_size: Optional[int],
+    ):
+        self.model = self.create_model(model, weight, p_threshold, s_threshold)
+
+        self.s3 = s3
+        self.db = db
+
+        self.data_queue_size = data_queue_size
+        self.pick_queue_size = pick_queue_size
+
+        self._run_id = self._put_run_data(
+            model=model,
+            weight=weight,
+            p_threshold=p_threshold,
+            s_threshold=s_threshold,
+            components_loaded=s3.components,
+            seisbench_version=seisbench.__version__,
+            weight_version=self.model.weights_version,
+        )
+
+    def _put_run_data(self, **kwargs: Any) -> ObjectId:
+        kwargs["timestamp"] = datetime.datetime.now(datetime.timezone.utc)
+        return self.db["sb_runs"].insert_one(kwargs).inserted_id
+
+    @staticmethod
+    def create_model(
+        model: str, weight: str, p_threshold: float, s_threshold: float
+    ) -> sbm.WaveformModel:
+        model = sbm.__getattribute__(model).from_pretrained(weight)
+        model.default_args["P_threshold"] = p_threshold
+        model.default_args["S_threshold"] = s_threshold
+        return model
+
+    def run(self):
+        asyncio.run(self._run_async())
+
+    async def _run_async(self):
+        data = asyncio.Queue(self.data_queue_size)
+        picks = asyncio.Queue(self.pick_queue_size)
+
+        task_load = self._load_data(data)
+        task_pick = self._pick_data(data, picks)
+        task_db = self._write_picks_to_db(picks)
+
+        await asyncio.gather(task_load, task_pick, task_db)
+
+    async def _load_data(
+        self,
+        data: asyncio.Queue[obspy.Stream | None],
+    ):
+        for stream in self.s3.load_data():
+            await data.put(stream)
+
+        await data.put(None)
+
     async def _pick_data(
         self,
         data: asyncio.Queue[obspy.Stream | None],
@@ -228,7 +252,7 @@ class S3MongoSBPicker:
             stream_annotations = await asyncio.to_thread(self.model.classify, stream)
             await picks.put(stream_annotations.picks)
 
-    async def _write_pick_to_db(self, picks: asyncio.Queue[sbu.PickList | None]):
+    async def _write_picks_to_db(self, picks: asyncio.Queue[sbu.PickList | None]):
         while True:
             stream_picks = await picks.get()
             if stream_picks is None:
@@ -245,14 +269,14 @@ class S3MongoSBPicker:
 
     def _write_single_picklist_to_db(self, picks: sbu.PickList):
         try:
-            self.collection.insert_many(
+            self.db["picks"].insert_many(
                 [
                     {
                         "trace_id": pick.trace_id,
                         "time": pick.peak_time.datetime,
                         "confidence": float(pick.peak_value),
                         "phase": pick.phase,
-                        **self.metadata,
+                        "run_id": self._run_id,
                     }
                     for pick in picks
                 ],
@@ -260,6 +284,11 @@ class S3MongoSBPicker:
             )
         except DuplicateKeyError:
             logger.warning("Some duplicate entries have been skipped.")
+        except BulkWriteError as e:
+            if all(x["code"] == 11000 for x in e.details["writeErrors"]):
+                logger.warning("Some duplicate entries have been skipped.")
+            else:
+                raise e
 
 
 if __name__ == "__main__":
