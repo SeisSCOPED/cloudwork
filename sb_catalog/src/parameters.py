aa549e3b38d0187bb601f536e5d14259954f3907
Yiyu Ni on EC2 <niyiyu@uw.edu>
2024-12-20 22:17:58 +0000

    Add placeholder for docdb uri and earthscope s3 access point
    Signed-off-by: Yiyu Ni on EC2 <niyiyu@uw.edu>

--- sb_catalog/src/parameters.py
+++ sb_catalog/src/parameters.py
@@ -6,3 +6,10 @@ Parameter for cloud job submission and DocumentDB access. All parameters are req
 JOB_QUEUE = ""
 JOB_DEFINITION_PICKING = ""
 JOB_DEFINITION_ASSOCIATION = ""
+
+# Required by DocumentDB access
+# please verify username, password, and location of the pem file
+DOCDB_ENDPOINT_URI = ""
+
+# Required by EarthScope S3 access
+EARTHSCOPE_S3_ACCESS_POINT = ""
